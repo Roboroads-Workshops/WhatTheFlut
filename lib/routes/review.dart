4c489469e6f28c4719de63a24c068ab88a460d7f
Robbin Schepers <robbin@deschepers.nl>
2022-03-22 23:58:23 +0100

    Step 8

--- lib/routes/review.dart
+++ lib/routes/review.dart
@@ -1,6 +1,6 @@
 import 'package:flutter/material.dart';
 
-class Review extends StatelessWidget {
+class Review extends StatefulWidget {
   final String title;
   final String body;
   final Image image;
@@ -13,17 +13,71 @@ class Review extends StatelessWidget {
   }) : super(key: key);
 
   @override
+  State<Review> createState() => _ReviewState();
+}
+
+class _ReviewState extends State<Review> {
+  String notes = '';
+
+  @override
   Widget build(BuildContext context) {
     return Scaffold(
+      floatingActionButton: FloatingActionButton(
+        onPressed: () => showEditNotesDialog(context),
+        child: const Icon(Icons.edit_note),
+      ),
       appBar: AppBar(
-        title: Text(title),
+        title: Text(widget.title),
       ),
       body: Column(
         children: [
-          image,
-          Text(body),
+          widget.image,
+          Padding(
+            padding: const EdgeInsets.all(8.0),
+            child: Text(widget.body),
+          ),
+          if (notes.isNotEmpty) ...[
+            Padding(
+              padding: const EdgeInsets.all(8.0),
+              child: Text(
+                "Notes",
+                style: TextStyle(fontSize: 24),
+              ),
+            ),
+            Padding(
+              padding: const EdgeInsets.all(8.0),
+              child: Text(notes!),
+            ),
+          ]
         ],
       ),
+    );
+  }
+
+  void showEditNotesDialog(BuildContext context) {
+    var notesController = TextEditingController(text: notes);
+    showDialog(
+      context: context,
+      builder: (BuildContext context) {
+        return AlertDialog(
+          title: Text("Edit notes"),
+          content: TextField(
+            controller: notesController,
+          ),
+          actions: [
+            TextButton(
+              onPressed: () {
+                setState(() {
+                  notes = notesController.text;
+                });
+
+                Navigator.of(context).pop();
+              },
+              child: Text("APPLY"),
+            ),
+          ],
+        );
+      },
     );
   }
 }
